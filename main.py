52daaf5befeeea724012acde9f7945d3e3487a65
7BEST7-77 <beste2010.2@gmail.com>
2025-07-17 00:02:31 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from segment_water import detect_water
+from forecast_flood import forecast_risk
+
+print("🌊 Welcome to FloodWatch-AI 🌊")
+print("Running water detection...")
+
+detect_water("data/sample.jpg")
+
+print("Running flood forecast...")
+forecast_risk()
